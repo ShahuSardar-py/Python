58df56c6b26c983b6dd752e5143e024fa276940b
Shahu Sardar <120025939+Shahu4@users.noreply.github.com>
2023-04-20 21:44:46 +0530

    Add files via upload

---
+++ Variables and data types.py
@@ -0,0 +1,24 @@
+a = 10
+
+b= 20.65
+
+c= 3+5j
+
+d= 'Shahu'
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+x= "Python is "
+y= "amazing"
+print(x, y)
+
+echo "# Python" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shahu4/Python.git
+git push -u origin main
